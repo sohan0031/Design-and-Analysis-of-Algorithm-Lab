57314f7b034d92a6129acc6c033960c4382d2719
sohan0031 <patilsohan0031@gmail.com>
2025-11-05 10:29:16 +0530

    Assignment 1 - Merge Sort
    Implemented a Merge Sort algorithm to sort customer orders based on timestamps efficiently. The solution is designed to handle up to 1 million orders with minimal computational overhead.

--- OrderSorting.java
+++ OrderSorting.java
@@ -1,3 +1,10 @@
+// Design and implement a sorting algorithm using Merge Sort to efficiently arrange customer 
+// orders based on their timestamps. The solution should handle a large dataset (up to 1 million 
+// orders) with minimal computational overhead. Additionally, analyze the time complexity and 
+// compare it with traditional sorting techniques.
+
+//Sohan Patil - 123B5F139
+
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -77,3 +84,4 @@ public class OrderSorting {
         printOrders(orders, 10);
     }
 }
+
