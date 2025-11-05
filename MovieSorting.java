7e4875615f1a746541f200ea3494c02fb81d5102
sohan0031 <patilsohan0031@gmail.com>
2025-11-05 10:30:53 +0530

    Assignment 2 Output - Quick Sort
    Added comments and optimized quicksort for movie sorting.

--- MovieSorting.java
+++ MovieSorting.java
@@ -1,3 +1,14 @@
+// Movie Recommendation System Optimization 
+// A popular OTT platform, StreamFlix, offers personalized recommendations by sorting movies 
+// based on user preferences, such as IMDB rating, release year, or watch time popularity. 
+// However, during peak hours, sorting large datasets slows down the system. 
+// As a backend engineer, you must: 
+// ● Implement Quicksort to efficiently sort movies based on various user-selected 
+// parameters. 
+// ● Handle large datasets containing of  movies while maintaining fast response times 
+
+// Sohan Patil - 123B5F139 
+
 import java.util.*;
 import java.time.Duration;
 import java.time.Instant;
@@ -99,3 +110,4 @@ public class MovieSorting {
         sc.close();
     }
 }
+
